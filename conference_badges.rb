bbd536aebc3bd6db4738f628b27267f9ccad3615
Christoph Gruener <christophgruener@me.com>
2019-02-13 18:14:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,11 @@ def batch_badge_creator(names_list)
   names_list.each {|name| badge_list << badge_maker(name)}
   badge_list
 end
+
+def assign_rooms(names_list)
+  room_counter = 1 
+  names_list.each do 
+    |name| "Hello, #{name}! You'll be assigned to room #{room_counter}!"
+    room_counter += 1 
+  end
+end
