a26892d5dd20808c08fa53de326603b4579e7806
Christoph Gruener <christophgruener@me.com>
2019-02-13 18:08:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,9 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(names_list)
+  badge_list = []
+  names_list.each do {|name| badge_list << badge_maker(name)}
+  badge_list
+end
