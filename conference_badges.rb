32efe02394b4c385d0994a972127059070a0c170
Christoph Gruener <christophgruener@me.com>
2019-02-13 18:09:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,6 +4,6 @@ end
 
 def batch_badge_creator(names_list)
   badge_list = []
-  names_list.each do {|name| badge_list << badge_maker(name)}
+  names_list.each {|name| badge_list << badge_maker(name)}
   badge_list
 end
